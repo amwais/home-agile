4f37768e8692ccdd72425685a6f95ab5bd3284f0
amirplanet <amir.waisman@planet.com>
2018-12-24 14:23:30 +0100

    deploying?

--- server.js
+++ server.js
@@ -37,6 +37,15 @@ app.use('/api/sprints', sprints);
 app.use('/api/tickets', tickets);
 app.use('/api/projects', projects);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
